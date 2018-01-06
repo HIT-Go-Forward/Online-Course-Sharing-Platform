6ae005b35d25b1d50ebb2554b91a80b076b07761
helloworld575 <zhichenli6@gmail.com>
2018-01-06 15:19:45 +0800

    adding the homepage file

--- online_course/account/urls.py
+++ online_course/account/urls.py
@@ -5,10 +5,12 @@ from account.views.login import login_authenticate
 from account.views.logout import logout_view
 from account.views.profile import profile_settings
 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^login/$', login_authenticate, name='login'),
     url(r'^logout/$', logout_view, name='logout'),
-    url(r'^settings/profile/$', profile_settings, name='profile_settings')
+    url(r'^settings/profile/$', profile_settings, name='profile_settings'),
+    url(r'^center/$',TemplateView.as_view(template_name='account/user.html'),name='center')
 ]
