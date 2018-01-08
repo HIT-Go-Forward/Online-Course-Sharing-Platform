c28d312515276563873c2480893239ac829dacc0
Moandor <moandor.y@gmail.com>
2018-01-08 16:35:18 +0800

    Add password change

--- online_course/account/urls.py
+++ online_course/account/urls.py
@@ -4,11 +4,13 @@ from account.views.register import register
 from account.views.login import login_authenticate
 from account.views.logout import logout_view
 from account.views.profile import profile_settings
+from account.views.change_password import change_password
 
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^login/$', login_authenticate, name='login'),
     url(r'^logout/$', logout_view, name='logout'),
-    url(r'^settings/profile/$', profile_settings, name='profile_settings')
+    url(r'^settings/profile/$', profile_settings, name='profile_settings'),
+    url(r'^settings/change_password/$', change_password, name='change_password'),
 ]
---
+++ online_course/account/views/change_password.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.urls import reverse_lazy
+from django.shortcuts import render
+
+
+@login_required(login_url=reverse_lazy('login'))
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'account/change_password.html', {'form': form})
