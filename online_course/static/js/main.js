840a7157f2db682f0437d58c201823dc84dadf64
helloworld575 <zhichenli6@gmail.com>
2018-01-16 14:52:46 +0800

    move page judge to main.js

---
+++ online_course/static/js/main.js
@@ -0,0 +1,22 @@
+function judge_page() {
+    list = document.location.href.split('/');
+    local = list[list.length - 2];
+    if (local === "profile") {
+        $("#profile-settings").addClass('active');
+    }
+    else if (local === "center") {
+        $("#center").addClass('active');
+    }
+    else if (local === "change_password") {
+        $("#change-password").addClass("active");
+    }
+    else if (local === "apply_teacher") {
+        $("#apply-teacher").addClass("active");
+    }
+    else if (local === "course_control") {
+        $("#course-control").addClass("active");
+    }
+    $("input[type='password']").addClass("input form-control");
+    $("input[type='text']").addClass("input form-control");
+    $("select").addClass("form-control")
+}
