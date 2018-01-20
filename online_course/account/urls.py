70dda4689795710d2d77cc90a3b600342ebdc8e7
helloworld575 <zhichenli6@gmail.com>
2018-01-20 17:44:41 +0800

    add temporary url to url.py

--- online_course/account/urls.py
+++ online_course/account/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from account.views.avatar import AvatarViewSet
@@ -19,4 +20,8 @@ urlpatterns = [
     url(r'^settings/profile/$', profile_settings, name='profile_settings'),
     url(r'^settings/change_password/$', change_password, name='change_password'),
     url(r'^center/$', profile_view, name='center'),
+    url(r'^teacher_apply',TemplateView.as_view(template_name='account/student/teacher_apply.html'),name="teacher_apply"),
+    url(r'^apply_manage',TemplateView.as_view(template_name='account/manager/apply_manage.html'),name="apply_manage"),
+    url(r'^course_control',TemplateView.as_view(template_name='account/teacher/course_control.html'),name="course_control"),
+    url(r'^my_course',TemplateView.as_view(template_name='account/student/my_course.html'),name="my_course"),
 ]
--- online_course/static/js/main.js
+++ online_course/static/js/main.js
@@ -1,23 +1,25 @@
 function judge_page() {
     list = document.location.href.split('/');
-    local = list[list.length - 2];
-    if (local === "profile") {
+    if ($.inArray("profile",list)>0) {
         $("#profile-settings").addClass('active');
     }
-    else if (local === "center") {
+    else if ($.inArray("center",list)>0) {
         $("#center").addClass('active');
     }
-    else if (local === "change_password") {
+    else if ($.inArray("change_password",list)>0) {
         $("#change-password").addClass("active");
     }
-    else if (local === "apply_teacher") {
+    else if ($.inArray("teacher_apply",list)>0) {
         $("#apply-teacher").addClass("active");
     }
-    else if (local === "course_control") {
+    else if ($.inArray("course_control",list)>0) {
         $("#course-control").addClass("active");
     }
-    else if(local==="apply_manage"){
+    else if($.inArray("apply_manage",list)>0){
         $("#apply-manage").addClass("active");
+    }
+    else if($.inArray("my_course",list)>0){
+        $("#my-course").addClass("active");
     }
     $("input[type='password']").addClass("input form-control");
     $("input[type='text']").addClass("input form-control");
