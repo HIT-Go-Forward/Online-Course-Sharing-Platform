fc9c6a4bc7be91e6c666f02af70650420ccd8143
Moandor <moandor.y@gmail.com>
2018-01-23 15:21:30 +0800

    Fix group check redirection

--- online_course/account/views/apply_manage.py
+++ online_course/account/views/apply_manage.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseBadRequest
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_safe
 
 from account.models import TeacherApplication
@@ -12,8 +13,8 @@ def manager_check(user):
     return user.groups.filter(name='Manager').exists()
 
 
-@user_passes_test(manager_check)
 @require_safe
+@user_passes_test(manager_check, login_url=reverse_lazy('login'))
 def apply_manage(request, page='1'):
     page = int(page)
     if page < 1 or 1000000000 < page:
