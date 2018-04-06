e9fb4848f4f70789446ff72ed1f58fcf62edfff0
helloworld575 <zhichenli6@gmail.com>
2018-04-06 11:33:26 +0800

    finish password judge part

--- online_course/static/js/main.js
+++ online_course/static/js/main.js
@@ -25,38 +25,7 @@ function judge_page() {
     $("input[type='text']").addClass("input form-control");
     $("select").addClass("form-control");
 }
-function password_judge(password1) {
-    var $pwd_bar_on = $(".password-judge");
-    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-    var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
-    var enoughRegex = new RegExp("(?=.{6,}).*", "g");
-    if (enoughRegex.test(password1) === false) {
-        $pwd_bar_on.removeClass('pwd-mid');
-        $pwd_bar_on.removeClass('pwd-weak');
-        $pwd_bar_on.removeClass('pwd-max');
-        $pwd_bar_on.addClass('pwd-fob');
-        return false;
-    }
-    else if (strongRegex.test(password1)) {
-        $pwd_bar_on.removeClass('pwd-mid');
-        $pwd_bar_on.removeClass('pwd-weak');
-        $pwd_bar_on.removeClass('pwd-fob');
-        $pwd_bar_on.addClass('pwd-max');
-    }
-    else if (mediumRegex.test(password1)) {
-        $pwd_bar_on.removeClass('pwd-fob');
-        $pwd_bar_on.removeClass('pwd-weak');
-        $pwd_bar_on.removeClass('pwd-max');
-        $pwd_bar_on.addClass('pwd-mid');
-    }
-    else {
-        $pwd_bar_on.removeClass('pwd-mid');
-        $pwd_bar_on.removeClass('pwd-fob');
-        $pwd_bar_on.removeClass('pwd-max');
-        $pwd_bar_on.addClass('pwd-weak');
-    }
-    return true;
-}
+
 
 function getCookie(name) {
     var cookieValue = null;
