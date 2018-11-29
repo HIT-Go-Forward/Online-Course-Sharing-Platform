e4abc96ac54eb5486d77ab97af94b34540168eb3
班耀强 <snx1788@gmail.com>
2018-11-29 22:25:48 +0800

    合并resource server项目

---
+++ resource-server/src/main/java/hit/to/go/controller/ResourceController.java
@@ -0,0 +1,52 @@
+package hit.to.go.controller;
+
+import hit.to.go.platform.protocol.RequestResults;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletContext;
+import java.io.File;
+
+/**
+ * Created by 班耀强 on 2018/10/18
+ */
+@Controller
+@ResponseBody
+@RequestMapping("/resource")
+public class ResourceController {
+    private ServletContext context;
+
+    public ResourceController(ServletContext context) {
+        this.context = context;
+    }
+
+    @RequestMapping("/upload")
+    public String upload(MultipartFile file, String storePath) {
+        storePath = context.getRealPath(storePath);
+        File storeFile = new File(storePath);
+
+        String result = "success";
+        if (storeFile.exists()) {
+            result = "override";
+            storeFile.delete();
+        }
+        storeFile.mkdirs();
+        try {
+            file.transferTo(storeFile);
+            return RequestResults.success(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return RequestResults.error("存储文件到本地发生异常" + e.getMessage());
+        }
+    }
+
+    @RequestMapping("/delete")
+    public String delete(String storePath) {
+        storePath = context.getRealPath(storePath);
+        File file = new File(storePath);
+        if (file.exists() && file.delete()) return RequestResults.success();
+        return RequestResults.error("文件不存在或已被删除");
+    }
+}
---
+++ resource-server/src/main/java/hit/to/go/platform/filter/RequestFilter.java
@@ -0,0 +1,50 @@
+package hit.to.go.platform.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by 班耀强 on 2018/9/20
+ */
+public class RequestFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+
+        String url = request.getRequestURI();
+        System.out.println(url);
+        if (url.endsWith(".media")) {
+            request.getRequestDispatcher(url.replace(".media", "")).forward(request, response);
+            return;
+        } else if (url.startsWith("/resource") && !url.endsWith(".action")) {
+            File file = new File(session.getServletContext().getRealPath(url));
+            if (file.exists()) {
+                FileInputStream inputStream = new FileInputStream(file);
+                OutputStream out = response.getOutputStream();
+                int data;
+                while ((data = inputStream.read()) != -1) out.write(data);
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
