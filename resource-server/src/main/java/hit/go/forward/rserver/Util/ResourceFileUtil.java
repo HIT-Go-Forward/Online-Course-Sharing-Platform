447d150471899be9e3a6db275a0a82d22f34fcf8
班耀强 <snx1788@gmail.com>
2018-12-03 13:42:03 +0800

    资源服务器改造0.01

---
+++ resource-server/src/main/java/hit/go/forward/rserver/Util/ResourceFileUtil.java
@@ -0,0 +1,13 @@
+package hit.go.forward.rserver.Util;
+
+import java.io.File;
+
+/**
+ * Created by 班耀强 on 2018/12/3
+ */
+public final class ResourceFileUtil {
+
+    public static void saveToLocalReplacePrev(File file) {
+
+    }
+}
